f9c10c07dcab104bd65000b36b36058f288aad65
fenixzava <fenixzava@gmail.com>
2023-07-09 18:42:00 +0300

    create Task4

---
+++ Task4.java
@@ -0,0 +1,20 @@
+//Разработайте программу, которая выбросит Exception, когда пользователь вводит пустую строку.
+// Пользователю должно показаться сообщение, что пустые строки вводить нельзя.
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Task4 {
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введите любые данные, кроме пустой строки: ");
+        try {
+            String result = reader.readLine();
+            if(result.equals("")) throw new RuntimeException("Пустую строку нельзя!");
+            System.out.println(result);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
